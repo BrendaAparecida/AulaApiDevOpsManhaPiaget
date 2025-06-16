bcf774e4f60fcd912f8fc68ac980511d4df35c36
brenda oliveira <brendaoliveiraf2003@gmnail.com>
2025-06-16 20:24:53 -0300

    API

---
+++ AulaApiDevOpsManhaPiaget/Models/Produtos.cs
@@ -0,0 +1,13 @@
+﻿namespace AulaApiDevOpsManhaPiaget.Models
+{
+    public class Produtos : Base
+    {
+        public string? Nome { get; set; }
+
+        public string? CNPJ { get; set; }
+
+        public string? Telefone { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
